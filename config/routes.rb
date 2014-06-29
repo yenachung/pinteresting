0509caba00fda9df6f515557b3b27b87d744c107
Yena Chung <yenachung@gmail.com>
2014-06-29 15:55:23 -0400

    Add pin/user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home" #path to home page is root_path
   get "about" => "pages#about" #this creates an about_path
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622214024) do
+ActiveRecord::Schema.define(version: 20140629182448) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
