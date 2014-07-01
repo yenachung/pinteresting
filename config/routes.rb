6b457b453aad4a8ccd5b506bcab6db8da27f3732
Yena Chung <yenachung@gmail.com>
2014-06-30 22:41:45 -0400

    Update show view, add glyphicon, and changed root path by making the pages Home a partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home" #path to home page is root_path
+  root "pins#index" #path to home page is root_path
   get "about" => "pages#about" #this creates an about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
